4bd551ef6acdebafa06c9a0c503c6401506c3c19
stepenshen <stepenshen@foxmail.com>
2020-03-27 11:07:15 +0800

    基于jdbc存储令牌

--- hello-spring-security-oauth2-server/src/main/java/com/funtl/spring/security/oauth2/server/configure/AuthorizationServerConfiguration.java
+++ hello-spring-security-oauth2-server/src/main/java/com/funtl/spring/security/oauth2/server/configure/AuthorizationServerConfiguration.java
@@ -1,11 +1,22 @@
 package com.funtl.spring.security.oauth2.server.configure;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+import javax.sql.DataSource;
 
 @Configuration
 //开启认证授权服务
@@ -16,22 +27,33 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
     @Autowired
     private BCryptPasswordEncoder passwordEncoder;
 
-    //客户端配置
 
+    //数据源配置
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        // 配置数据源（注意，我使用的是 HikariCP 连接池），以上注解是指定数据源，否则会有冲突
+        return DataSourceBuilder.create().build();
+    }
+    @Bean
+    public TokenStore tokenStore() {
+        // 基于 JDBC 实现，令牌保存到数据库
+        return new JdbcTokenStore(dataSource());
+    }
+    @Bean
+    public ClientDetailsService jdbcClientDetailsService() {
+        // 基于 JDBC 实现，需要事先在数据库配置客户端信息
+        return new JdbcClientDetailsService(dataSource());
+    }
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        // 设置令牌存储模式
+        endpoints.tokenStore(tokenStore());
+    }
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients
-                //在内存中配置客户端
-                .inMemory()
-                // client_id
-                .withClient("client")
-                // client_secret
-                .secret(passwordEncoder.encode("secret"))
-                //授权模式,这里使用了授权码模式
-                .authorizedGrantTypes("authorization_code")
-                //授权范围
-                .scopes("app")
-                //注册回调地址
-                .redirectUris("https://www.baidu.com");
+        // 客户端配置
+        clients.withClientDetails(jdbcClientDetailsService());
     }
 }
